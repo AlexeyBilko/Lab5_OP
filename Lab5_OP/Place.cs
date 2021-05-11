c26c5d05bc19ae2897406ce646cb7dca593ddecd
Alexey Bilko <bilkoalexey67@gmail.com>
2021-05-11 19:04:37 +0300

    added class place

---
+++ Lab5_OP/Place.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_OP
+{
+    class Place
+    {
+        double Latitude { get; set; }
+        double Longitude { get; set; }
+
+        string Type { get; set; }
+        string SubType { get; set; }
+        string Title { get; set; }
+        string Address { get; set; }
+
+        public Place(double latitude, double longitude, string type, string subtype, string title, string address)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+            Type = type;
+            SubType = subtype;
+            Title = title;
+            Address = address;
+        }
+    }
+}
