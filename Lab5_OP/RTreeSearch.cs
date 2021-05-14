af6344048b1a01bf7e2ccb325fbaf77addcc8034
Sergey <serggrom2002@gmail.com>
2021-05-14 13:58:05 +0200

    add resource file and output result

--- Lab5_OP/RTreeSearch.cs
+++ Lab5_OP/RTreeSearch.cs
@@ -8,33 +8,6 @@ namespace Lab5_OP
 {
     class RTreeSearch
     {
-        public static (Place, double) FindNearestPlaceBySubtype(RTree node, double x, double y)
-        {
-            int radius = 10;
-            List<Place> entities = FindNearestEntities(node, x, y, radius);
-            while ((entities == null || entities.Count == 0) && radius < 1000)
-            {
-                radius *= 2;
-                entities = FindNearestEntities(node, x, y, radius);
-
-            }
-            if (entities == null) return (null, -1);
-            return FindNearestOfTheList(entities, x, y);
-        }
-        public static (Place, double) FindNearestOfTheList(List<Place> entites, double x, double y)
-        {
-            Place nearest = null;
-            double minimalLength = 1000;
-            foreach (var Place in entites)
-            {
-                if (FindDistanceToPlace(Place, x, y) < minimalLength)
-                {
-                    nearest = Place;
-                    minimalLength = FindDistanceToPlace(Place, x, y);
-                }
-            }
-            return (nearest, minimalLength);
-        }
         public static List<Place> FindNearestEntities(RTree node, double x, double y, int radius)
         {
             List<Place> entities = new List<Place>();
@@ -44,25 +17,41 @@ namespace Lab5_OP
                 var second = (IsRadiusIsEnough(node.SecondChild, x, y, radius) ? FindNearestEntities(node.SecondChild, x, y, radius) : null);
               
                 
-                if (first == null && second == null) return null;
-                if (first == null) return second;
-                if (second == null) return first;
-                second.ForEach(item => first.Add(item));
+                if (first == null && second == null) 
+                    return null;
+                if (first == null) 
+                    return second;
+                if (second == null) 
+                    return first;
+
+                foreach (var item in second)
+                    first.Add(item);
+
                 return first;
             }
             foreach (var Place in node.list)
-            {
                 if (FindDistanceToPlace(Place, x, y) <= radius)
-                {
                     entities.Add(Place);
-                }
-            }
+            
             return entities;
         }
         static bool IsRadiusIsEnough(RTree node, double x, double y, int radius)
         {
-            double nearestX = x < node.latitudeMin ? node.latitudeMin : x > node.latitudeMax ? node.latitudeMax : x;
-            double nearestY = y < node.longitudeMin ? node.longitudeMin : x > node.longitudeMax ? node.longitudeMax : y;
+            double nearestX;
+            if (x < node.latitudeMin)
+                nearestX = node.latitudeMin;
+            if (x > node.latitudeMax)
+                nearestX = node.latitudeMax;
+            nearestX = x;
+
+
+            double nearestY;
+            if (y < node.longitudeMin)
+                nearestY = node.longitudeMin;
+            if (x > node.longitudeMax)
+                nearestY = node.longitudeMax;
+            nearestY = y;
+
             double xCoef1 = 111 * Math.Cos(y);
             double xCoef2 = 111 * Math.Cos(nearestY);
             double yCoef = 111;
